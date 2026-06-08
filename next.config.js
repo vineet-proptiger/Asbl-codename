4dba2720cc5fbcc413b7f7dabada163c1f39fe37
vineet-proptiger <vineet.mishra@proptiger.com>
2026-06-08 14:22:04 +0530

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/new-projects-in-hyderabad',
+        destination: '/new-launch-hyderabad',
+      },
+      {
+        source: '/new-projects-in-hyderabad/privacy-policy',
+        destination: '/new-launch-hyderabad/privacy-policy',
+      },
+    ]
+  },
   webpack: (config, { dev }) => {
     if (dev) config.cache = false
     return config
